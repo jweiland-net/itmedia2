17d76e480dc06f901726f977b0bc4d36daba7d9d
Stefan Froemken <froemken@gmail.com>
2018-10-31 15:30:15 +0100

    Update to version 1.2.0. Add requirement to maps2 4.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,23 +4,26 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'JWeiland.' . $_EXTKEY,
-    'Directory',
-    [
-        'Company' => 'list, show, search'
-    ],
-    // non-cacheable actions
-    [
-        'Company' => 'search'
-    ]
-);
+call_user_func(function($extKey) {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'JWeiland.' . $extKey,
+        'Directory',
+        [
+            'Company' => 'list, show, search'
+        ],
+        // non-cacheable actions
+        [
+            'Company' => 'search'
+        ]
+    );
 
-// use hook to automatically add a map record to current yellow page
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JWeiland\Itmedia2\Hooks\Tca\CreateMap::class;
+    // use hook to automatically add a map record to current yellow page
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JWeiland\Itmedia2\Hooks\Tca\CreateMap::class;
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\JWeiland\Itmedia2\Tasks\Update::class] = [
-    'extension'        => $_EXTKEY,
-    'title'            => 'Update itmedia2',
-    'description'      => 'Hide all itmedia2 records which are older than the specified age.'
-];
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\JWeiland\Itmedia2\Tasks\Update::class] = [
+        'extension'        => $extKey,
+        'title'            => 'Update itmedia2',
+        'description'      => 'Hide all itmedia2 records which are older than the specified age.'
+    ];
+
+}, $_EXTKEY);
--- ext_tables.php
+++ ext_tables.php
@@ -3,31 +3,33 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'JWeiland.' . $_EXTKEY,
-    'Directory',
-    'LLL:EXT:itmedia2/Resources/Private/Language/locallang_db.xlf:plugin.title'
-);
+call_user_func(function($extKey) {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'JWeiland.' . $extKey,
+        'Directory',
+        'LLL:EXT:itmedia2/Resources/Private/Language/locallang_db.xlf:plugin.title'
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'IT and Media');
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_directory'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $_EXTKEY . '_directory',
-    'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/YellowPages.xml'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'IT and Media');
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$extKey . '_directory'] = 'pi_flexform';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        $extKey . '_directory',
+        'FILE:EXT:' . $extKey . '/Configuration/FlexForms/YellowPages.xml'
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_itmedia2_domain_model_company',
-    'EXT:itmedia2/Resources/Private/Language/locallang_csh_tx_itmedia2_domain_model_company.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_itmedia2_domain_model_company');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'tx_itmedia2_domain_model_company',
+        'EXT:itmedia2/Resources/Private/Language/locallang_csh_tx_itmedia2_domain_model_company.xlf'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_itmedia2_domain_model_company');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_itmedia2_domain_model_district',
-    'EXT:itmedia2/Resources/Private/Language/locallang_csh_tx_itmedia2_domain_model_district.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_itmedia2_domain_model_district');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'tx_itmedia2_domain_model_district',
+        'EXT:itmedia2/Resources/Private/Language/locallang_csh_tx_itmedia2_domain_model_district.xlf'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_itmedia2_domain_model_district');
 
-$extConf = unserialize($_EXTCONF);
-$tsConfig = 'ext.itmedia2.pid = ' . (int) $extConf['poiCollectionPid'];
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($tsConfig);
+    $extConf = unserialize($extKey);
+    $tsConfig = 'ext.itmedia2.pid = ' . (int) $extConf['poiCollectionPid'];
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($tsConfig);
+}, $_EXTKEY);
