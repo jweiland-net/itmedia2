726efcc7068291f2408975a75b1a4b7a355b1e13
Stefan Froemken <froemken@gmail.com>
2018-11-02 09:51:11 +0100

    Add missing icon for sys_category

--- ext_tables.sql
+++ ext_tables.sql
@@ -27,7 +27,6 @@ CREATE TABLE tx_itmedia2_domain_model_company (
 	facebook varchar(255) DEFAULT '' NOT NULL,
 	twitter varchar(255) DEFAULT '' NOT NULL,
 	google varchar(255) DEFAULT '' NOT NULL,
-	tx_maps2_uid int(11) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
