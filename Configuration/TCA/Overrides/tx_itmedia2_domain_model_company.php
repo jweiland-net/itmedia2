17dfd4e8ce7344cd034f1d52592d538e809bce19
Stefan Froemken <froemken@gmail.com>
2018-11-20 08:38:59 +0100

    Use Maps2 Registry instead of CreateMap class

--- Configuration/TCA/Overrides/tx_itmedia2_domain_model_company.php
+++ Configuration/TCA/Overrides/tx_itmedia2_domain_model_company.php
@@ -1,35 +1,49 @@
 <?php
-call_user_func(function() {
-    // Add tx_maps2_uid column to company table
-    $result = \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
-        'itmedia2',
-        'tx_itmedia2_domain_model_company'
-    );
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
+// Add tx_maps2_uid column to company table
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
+    \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
         'itmedia2',
         'tx_itmedia2_domain_model_company',
-        'main_trade',
         [
-            'label' => 'LLL:EXT:yellowpages2/Resources/Private/Language/locallang_db.xlf:tx_yellowpages2_domain_model_company.mainTrade',
-            'fieldConfiguration' => [
-                'minitems' => 0,
-                'maxitems' => 1,
-                'eval' => 'required'
+            'addressColumns' => ['street', 'house_number', 'zip', 'city'],
+            'defaultCountry' => 'Deutschland',
+            'defaultStoragePid' => [
+                'extKey' => 'itmedia2',
+                'property' => 'poiCollectionPid'
+            ],
+            'synchronizeColumns' => [
+                [
+                    'foreignColumnName' => 'company',
+                    'poiCollectionColumnName' => 'title'
+                ]
             ]
         ]
     );
+}
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-        'itmedia2',
-        'tx_itmedia2_domain_model_company',
-        'trades',
-        [
-            'label' => 'LLL:EXT:yellowpages2/Resources/Private/Language/locallang_db.xlf:tx_yellowpages2_domain_model_company.trades',
-            'fieldConfiguration' => [
-                'minitems' => 0,
-                'maxitems' => 2
-            ]
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
+    'itmedia2',
+    'tx_itmedia2_domain_model_company',
+    'main_trade',
+    [
+        'label' => 'LLL:EXT:yellowpages2/Resources/Private/Language/locallang_db.xlf:tx_yellowpages2_domain_model_company.mainTrade',
+        'fieldConfiguration' => [
+            'minitems' => 0,
+            'maxitems' => 1,
+            'eval' => 'required'
         ]
-    );
-});
+    ]
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
+    'itmedia2',
+    'tx_itmedia2_domain_model_company',
+    'trades',
+    [
+        'label' => 'LLL:EXT:yellowpages2/Resources/Private/Language/locallang_db.xlf:tx_yellowpages2_domain_model_company.trades',
+        'fieldConfiguration' => [
+            'minitems' => 0,
+            'maxitems' => 2
+        ]
+    ]
+);
