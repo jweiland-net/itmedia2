051abc18f964e27fc1db3f8083d285bb02cd3f4f
Stefan Froemken <froemken@gmail.com>
2018-11-20 08:40:01 +0100

    Update to version 1.2.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,26 +4,20 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function($extKey) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'JWeiland.' . $extKey,
-        'Directory',
-        [
-            'Company' => 'list, show, search'
-        ],
-        // non-cacheable actions
-        [
-            'Company' => 'search'
-        ]
-    );
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'JWeiland.itmedia2',
+    'Directory',
+    [
+        'Company' => 'list, show, search'
+    ],
+    // non-cacheable actions
+    [
+        'Company' => 'search'
+    ]
+);
 
-    // use hook to automatically add a map record to current yellow page
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JWeiland\Itmedia2\Hooks\Tca\CreateMap::class;
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\JWeiland\Itmedia2\Tasks\Update::class] = [
-        'extension'        => $extKey,
-        'title'            => 'Update itmedia2',
-        'description'      => 'Hide all itmedia2 records which are older than the specified age.'
-    ];
-
-}, $_EXTKEY);
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\JWeiland\Itmedia2\Tasks\Update::class] = [
+    'extension'        => 'itmedia2',
+    'title'            => 'Update itmedia2',
+    'description'      => 'Hide all itmedia2 records which are older than the specified age.'
+];
