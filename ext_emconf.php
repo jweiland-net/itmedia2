d5e3b295a2814a2df8cf2852abe31a371f5124a9
Stefan Froemken <froemken@gmail.com>
2019-06-27 17:42:13 +0200

    Update dependency of yellowpages to 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
             'maps2' => '5.0.0-5.99.99',
-            'yellowpages2' => '2.2.0-2.99.99'
+            'yellowpages2' => '3.0.0-3.99.99'
         ],
         'conflicts' => [],
         'suggests' => []
