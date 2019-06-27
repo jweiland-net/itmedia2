d3f0eb19c490103df2a07d3502604522ad4c1ebe
Stefan Froemken <froemken@gmail.com>
2019-06-27 15:51:48 +0200

    Update dependencies to maps and TYPO3

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
             'maps2' => '5.0.0-5.99.99',
             'yellowpages2' => '2.2.0-2.99.99'
         ],
