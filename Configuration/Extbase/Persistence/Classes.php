0bd5df1b9ef20893be8a3ac37d1941f5e4680de3
Stefan Froemken <froemken@gmail.com>
2020-11-16 15:10:06 +0100

    Implement new extbase table mapping

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    \JWeiland\Itmedia2\Domain\Model\Category::class => [
+        'tableName' => 'sys_category',
+    ],
+    \JWeiland\Itmedia2\Domain\Model\District::class => [
+        'tableName' => 'tx_yellowpages2_domain_model_district',
+    ],
+];
