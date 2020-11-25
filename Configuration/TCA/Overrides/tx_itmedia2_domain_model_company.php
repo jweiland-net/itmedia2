01efde64e2d3e755c216deca60c2ebb30559775a
Stefan Froemken <froemken@gmail.com>
2020-11-25 09:55:57 +0100

    Use checkboxToggle as renderType

--- Configuration/TCA/Overrides/tx_itmedia2_domain_model_company.php
+++ Configuration/TCA/Overrides/tx_itmedia2_domain_model_company.php
@@ -30,7 +30,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
     'tx_itmedia2_domain_model_company',
     'main_trade',
     [
-        'label' => 'LLL:EXT:yellowpages2/Resources/Private/Language/locallang_db.xlf:tx_yellowpages2_domain_model_company.mainTrade',
+        'label' => 'LLL:EXT:itmedia2/Resources/Private/Language/locallang_db.xlf:tx_itmedia2_domain_model_company.mainTrade',
         'fieldConfiguration' => [
             'minitems' => 0,
             'maxitems' => 1,
@@ -44,7 +44,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
     'tx_itmedia2_domain_model_company',
     'trades',
     [
-        'label' => 'LLL:EXT:yellowpages2/Resources/Private/Language/locallang_db.xlf:tx_yellowpages2_domain_model_company.trades',
+        'label' => 'LLL:EXT:itmedia2/Resources/Private/Language/locallang_db.xlf:tx_itmedia2_domain_model_company.trades',
         'fieldConfiguration' => [
             'minitems' => 0,
             'maxitems' => 2
