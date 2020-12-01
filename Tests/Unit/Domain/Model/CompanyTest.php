ea0576f6fd0c37a14a30bd507d9342d5684e327f
Stefan Froemken <froemken@gmail.com>
2020-12-01 15:30:12 +0100

    Add getter for first logo

--- Tests/Unit/Domain/Model/CompanyTest.php
+++ Tests/Unit/Domain/Model/CompanyTest.php
@@ -123,7 +123,28 @@ class CompanyTest extends UnitTestCase
     /**
      * @test
      */
-    public function getLogoInitiallyReturnsObjectStorage()
+    public function getLogoInitiallyReturnsEmptyArray()
+    {
+        self::assertSame(
+            [],
+            $this->subject->getLogo()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getFirstLogoInitiallyReturnsNull()
+    {
+        self::assertNull(
+            $this->subject->getFirstLogo()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getOriginalLogoInitiallyReturnsObjectStorage()
     {
         self::assertEquals(
             new ObjectStorage(),
