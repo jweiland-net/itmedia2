27348f1aec251d1352c258006f1b9f822d452af5
Stefan Froemken <froemken@gmail.com>
2022-05-09 14:30:57 +0200

    Update to version 3.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,12 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '3.0.12',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.20-10.4.99',
-            'glossary2' => '4.1.0-4.99.99',
-            'yellowpages2' => '4.0.0-5.99.99'
+            'glossary2' => '5.0.0-0.0.0',
+            'yellowpages2' => '4.0.0-0.0.0'
         ],
         'conflicts' => [],
         'suggests' => [
